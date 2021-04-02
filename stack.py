cdf88c111b7b0c341f15eac51181c1696cd2353c
rajesh0025 <70711676+rajesh0025@users.noreply.github.com>
2021-04-02 21:49:36 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,45 @@
+#list are not effective in stacks because of unwanted memory allocation so collections deque were used instead
+
+"""from collections import deque
+s=deque()
+s.append("one")
+s.append("two")
+s.append("three")
+print(s)
+"""
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.container = deque()
+    
+    def push(self,val):
+        self.container.append(val)
+        
+    def pop(self):
+        return self.container.pop()
+    
+    def peek(self):
+        return  self.container[-1]
+    
+    def is_empty(self):
+        return len(self.container)==0
+    
+    def size(self):
+        return len(self.container)
+
+
+def reverse(string):
+    stack=Stack()
+    
+    for r in string:
+        stack.push(r)
+    rev=''
+    while stack.size()!=0:
+        rev+=stack.pop()
+
+    return rev 
+
+
+if __name__ == '__main__':
+    print(reverse("We will conquere COVI-19"))
+    print(reverse("I am the king"))
